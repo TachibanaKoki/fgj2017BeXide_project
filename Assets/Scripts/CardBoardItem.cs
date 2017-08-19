3b31efab98fab6a2faff2a8e01aa9b4d8e68aafb
tatmos <charytanaka@gmail.com>
2017-08-19 16:32:59 +0900

    ACF ACB　とその他ファイルコミット

---
+++ Assets/Scripts/CardBoardItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBoardItem : MonoBehaviour
+{
+    public CardBoardParts partsType;
+    [HideInInspector]
+    public Texture texture;
+
+	void Start ()
+    {
+        texture = GetComponent<Renderer>().material.mainTexture;
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/CardboardController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardBoardParts
+{
+    Head,
+    Body,
+    Hip,
+    LeftArm,
+    RightArm,
+}
+
+
+public class CardboardController : MonoBehaviour
+{
+    [Tooltip("Head,body,hip,leftarm,rightarm")]
+    public GameObject[] Parts;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.tag == "CardBoard")
+        {
+            PickCardBoard(col.gameObject.GetComponent<CardBoardItem>());
+            Destroy(col.gameObject);
+        }
+    }
+
+    void PickCardBoard(CardBoardItem item)
+    {
+        Parts[(int)item.partsType].GetComponent<Renderer>().material.mainTexture = item.texture;
+    }
+}
