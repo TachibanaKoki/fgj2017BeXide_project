66f35d44cf82a6993239cb5655058d366ebacd54
penguinsunited <USSTylenol@gmail.com>
2017-08-20 07:43:28 +0900

    no message

---
+++ Assets/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Time.timeSinceLevelLoad > 4 && SceneManager.GetActiveScene ().buildIndex != 9)
+			SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex + 1);
+		else if (SceneManager.GetActiveScene ().buildIndex == 9 && Input.GetButton ("Fire1"))
+			SceneManager.LoadScene("Title");
+	}
+}
---
+++ Assets/Prologue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Prologue : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Time.timeSinceLevelLoad > 4 && SceneManager.GetActiveScene().buildIndex != 6)
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
+
