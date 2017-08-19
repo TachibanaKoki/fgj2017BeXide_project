929839d282a9c706351b15ddc2ff17587d21e3ec
KoukiTachibana <standchat0820@gmail.com>
2017-08-19 21:45:35 +0900

    サウンド追加版

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,14 +8,15 @@ public class GameManager : MonoBehaviour
     public static GameManager I;
 
     public bool isGameClear = false;
-
     public void Start()
     {
         I = this;
-        if(SoundManager.Instance==null)
+        
+        if (SoundManager.Instance==null)
         {
             SceneManager.LoadSceneAsync("SoundSetupScene", LoadSceneMode.Additive);
         }
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("main"));
     }
 
 	public void GameClear()
