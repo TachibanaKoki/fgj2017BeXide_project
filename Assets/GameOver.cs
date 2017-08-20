090e5c4fca837e828ab64205d41755408391a0cb
penguinsunited <USSTylenol@gmail.com>
2017-08-20 15:45:46 +0900

    game over 修正

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -15,7 +15,7 @@ public class GameOver : MonoBehaviour {
 	
 		if (Time.timeSinceLevelLoad > 4 && SceneManager.GetActiveScene ().buildIndex != 8)
 			SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex + 1);
-		else if (SceneManager.GetActiveScene ().buildIndex == 9 && Input.GetButton ("Fire1"))
+		else if (SceneManager.GetActiveScene ().buildIndex == 8 && Input.GetButton ("Fire1"))
 			SceneManager.LoadScene("Title");
 	}
 }
