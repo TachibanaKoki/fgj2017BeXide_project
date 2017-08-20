43777cb1343f70c003289d68d988edb6c639b758
penguinsunited <USSTylenol@gmail.com>
2017-08-20 13:49:13 +0900

    clear

---
+++ Assets/fadeout.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fadeout : MonoBehaviour {
+
+	public Image image;
+	float alp = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		image.color = new Color (0, 0, 0, alp);
+		alp += 5;
+		if (alp == 255) {
+			alp -= 10;
+		}
+		
+	}
+}
