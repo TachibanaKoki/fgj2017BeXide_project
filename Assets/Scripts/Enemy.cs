a81046d058acc34ad448dccb36dc6523978c8070
Hiroaki Ogasawara <hiroga1030@gmail.com>
2017-08-20 09:37:11 +0900

    Policeのprefabを更新したはず...git初心者ですいません！

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,94 +5,98 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    CharacterStatus state;
+	CharacterStatus state;
 
-    GameObject character;
-    
-    public bool isStop = false;
+	GameObject character;
 
-    public Transform[] points;
-    private int destPoint = 0;
-    NavMeshAgent m_agent;
+	public bool isStop = false;
 
-    // Use this for initialization
-    void Start ()
-    {
-        state = CharacterStatus.Normal;
-        if(!isStop)
-        {
-            m_agent = GetComponent<NavMeshAgent>();
-        }
-        points = NavPointsData.I.points;
+	public Transform[] points;
+	private int destPoint = 0;
+	NavMeshAgent m_agent;
+
+	public TextMesh tm;
+
+	// Use this for initialization
+	void Start ()
+	{
+		state = CharacterStatus.Normal;
+		if(!isStop)
+		{
+			m_agent = GetComponent<NavMeshAgent>();
+		}
+		points = NavPointsData.I.points;
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
-    {
-        switch (state)
-        {
-            case CharacterStatus.Normal:
-                if(!isStop)
-                {
-                    if (m_agent.remainingDistance < 0.5f)
-                        GotoNextPoint();
-                }
+	{
+		switch (state)
+		{
+		case CharacterStatus.Normal:
+			if(!isStop)
+			{
+				if (m_agent.remainingDistance < 0.5f)
+					GotoNextPoint();
+			}
 
-                break;
-            case CharacterStatus.Discovery:
-                if (isStop)
-                {
-                    transform.LookAt(character.transform);
-                }
-                else
-                {
-                    m_agent.destination = character.transform.position;
-                }
+			break;
+		case CharacterStatus.Discovery:
+			if (isStop)
+			{
+				transform.LookAt(character.transform);
+			}
+			else
+			{
+				m_agent.destination = character.transform.position;
+			}
 
-                break;
-        }
+			break;
+		}
 
 	}
 
-    void GotoNextPoint()
-    {
-        // Returns if no points have been set up
-        if (NavPointsData.I.points.Length == 0)
-            return;
+	void GotoNextPoint()
+	{
+		// Returns if no points have been set up
+		if (NavPointsData.I.points.Length == 0)
+			return;
 
-        // Set the agent to go to the currently selected destination.
-        m_agent.destination = points[destPoint].position;
+		// Set the agent to go to the currently selected destination.
+		m_agent.destination = points[destPoint].position;
 
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
-        destPoint = (destPoint + 1) % points.Length;
-    }
-
-
-    public void OnTriggerEnter(Collider col)
-    {
-        if (col.tag == "Player")
-        {
-            col.GetComponent<CharacterManager>().SetStatus(CharacterStatus.Discovery);
-            state = CharacterStatus.Discovery;
-            character = col.gameObject;
-            SoundManager.Instance.SoundEvent(SoundManager.EnumBgmEvent.bgm_hi);
-        }
-    }
+		// Choose the next point in the array as the destination,
+		// cycling to the start if necessary.
+		destPoint = (destPoint + 1) % points.Length;
+	}
 
 
-    void OnTriggerExit(Collider col)
-    {
-        if (col.tag == "Player")
-        {
-            col.GetComponent<CharacterManager>().SetStatus(CharacterStatus.Normal);
-            state = CharacterStatus.Normal;
-            if (!isStop)
-            {
-                GotoNextPoint();
-            }
-            SoundManager.Instance.SoundEvent(SoundManager.EnumBgmEvent.bgm_low);
-        }
-    }
+	public void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player")
+		{
+			col.GetComponent<CharacterManager>().SetStatus(CharacterStatus.Discovery);
+			state = CharacterStatus.Discovery;
+			character = col.gameObject;
+			SoundManager.Instance.SoundEvent(SoundManager.EnumBgmEvent.bgm_hi);
+			tm.gameObject.SetActive (true);
+		}
+	}
+
+
+	void OnTriggerExit(Collider col)
+	{
+		if (col.tag == "Player")
+		{
+			col.GetComponent<CharacterManager>().SetStatus(CharacterStatus.Normal);
+			state = CharacterStatus.Normal;
+			if (!isStop)
+			{
+				GotoNextPoint();
+			}
+			SoundManager.Instance.SoundEvent(SoundManager.EnumBgmEvent.bgm_low);
+			tm.gameObject.SetActive (false);
+		}
+	}
 
 }
