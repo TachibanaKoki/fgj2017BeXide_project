86251ad85fe99f76e3710a295d228791fd2e6310
KoukiTachibana <standchat0820@gmail.com>
2017-08-20 15:30:12 +0900

    ビルドエラー修正

--- Assets/fadeout.cs
+++ Assets/fadeout.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class fadeout : MonoBehaviour {
 
