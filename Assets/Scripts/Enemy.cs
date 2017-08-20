b0b8aee2699012deb9338687edc7efa47e09db2c
tatmos <charytanaka@gmail.com>
2017-08-20 13:28:40 +0900

    エネミーセリフ組み込み

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -117,60 +117,60 @@ public class Enemy : MonoBehaviour
 				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
 			} else if (message == 2) {
 				serifu = "裸じゃねぇか？";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_hadakaja");
 			} else if (message == 3) {
 				serifu = "ここに変態がいるぞい！！";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_kokonihen");
 			} else if (message == 4) {
 				serifu = "やばい！不審者！";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_yabai_fusinsya");
 			} else if (message == 5) {
 				serifu = "ギャァァァァ！！";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_gyaa");
 			} else {
 				serifu = "ワイジャパニーズピーポー？";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_wahtsjapanipeapo");
 			}
 		}
 		else if(m_nakedGage > 0.25 && m_nakedGage < 0.49) 
 		{
 			if (message == 1) {
 				serifu = "あれ？！";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_are");
 			} else if (message == 2) {
 				serifu = "Oh my God!!";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_omyga");
 			} else if (message == 3) {
 				serifu = "What the…?";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_what");
 			} else if (message == 4) {
 				serifu = "お兄さん、どうした？";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_onisandosita");
 			} else if (message == 5) {
 				serifu = "ほんまに？";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_honmani");
 			} else {
 				serifu = "一体だれ。。。？";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_ittaidare");
 			}
 		}
 		else if (m_nakedGage > 0.5 && m_nakedGage < 0.74) 
 		{
 			if (message == 1) {
 				serifu = "何それ？";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_nanisore");
 			} else if (message == 2) {
 				serifu = "あのやつがあやしい。。。";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_anoyatugaayashi");
 			} else if (message == 3) {
 				serifu = "お前さん、何としとるんじゃ？";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_omaesannanisitorunjya");
 			} else if (message == 4) {
 				serifu = "えぇぇぇぇ、何？";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_eeee_nani");
 			} else if (message == 5) {
 				serifu = "何じゃ？あの人影は。。。";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_nanjyaanohitokagewa");
 			} else {
 				serifu = "。。。。。。。。？";
 			}
@@ -179,22 +179,22 @@ public class Enemy : MonoBehaviour
 		{
 			if (message == 1) {
 				serifu = "。。。まぁ、ファションかな？";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_maafashionkana");
 			} else if (message == 2) {
 				serifu = "若者がわかりません。";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_wakamonogawakarimasen");
 			} else if (message == 3) {
 				serifu = "ララララララ。。。。";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_lalalalalalala");
 			} else if (message == 4) {
 				serifu = "今日もいい天気じゃ！";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_kyomoiitenkijya");
 			} else if (message == 5) {
 				serifu = "あ、またわしの足が暴れとるわい。";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_a_matawashinoashiga");
 			} else {
 				serifu = "家に帰って笑点でも見ようか？";
-				SoundManager.Instance.PlayEnemyVoice ("v_hentai");
+				SoundManager.Instance.PlayEnemyVoice ("v_ienikaetteshotendemo");
 			}
 		}
 
